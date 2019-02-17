f7026f74ee71636bac8d1ceefd5a3dab49519a4e
Heartsonsunday <leahgdg@hotmail.com>
2019-02-17 10:40:36 -0500

    reorganized

---
+++ JS_algorithmsDataStructs/eS6/mutateConst_Array.js
@@ -0,0 +1,12 @@
+const s = [5, 7, 2];
+function editInPlace() {
+  "use strict";
+  // change code below this line
+
+  s[0] = 2; //<- this is invalid
+s[1] = 5;
+s[2] = 7;
+console.log(s);
+  // change code above this line
+}
+editInPlace();
