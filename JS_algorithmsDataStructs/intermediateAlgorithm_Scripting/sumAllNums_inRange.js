34dca48f6aaa8016ea41c9b60e4f3560869bc556
Leah-Grace <leahgdg@hotmail.com>
2019-06-11 20:31:16 -0400

    refractored and solved

--- JS_algorithmsDataStructs/intermediateAlgorithm_Scripting/sumAllNums_inRange.js
+++ JS_algorithmsDataStructs/intermediateAlgorithm_Scripting/sumAllNums_inRange.js
@@ -1,35 +1,17 @@
 function sumAll(arr) {
-  //Identify components
   let newArr = arr.sort(function(a, b) {
     return a - b;
   });
+  var initialNum = newArr[0];
+  var finalNum = newArr[1];
   var sum = 0;
-  //console.log(`Value Starts At ${newArr[0]}`);
-  for (var i = newArr[0]; i < newArr[1] + 1; i++) {
-    sum += Number(i);
-    // console.log(`The sum is now ${sum}`);
+  for (var i = initialNum; i <= finalNum; i++) {
+    sum += i;
   }
   console.log(sum);
   return sum;
 }
-/*
-  function sumRange(less, greater) {
-    var sum = less;
-    counter = greater - less;
-    for (counter; counter > 0; counter--) {
-      let addition = less;
-      sum += Number(addition + counter);
-    }
-    console.log(sum);
-    return sum;
-  }
-  //sort numbers from the array
-  if (num1 > num2) {
-    sumRange(num2, num1);
-  } else {
-    sumRange(num1, num2);
-  }
-*/
+
 sumAll([1, 4]);
 sumAll([10, 4]);
 sumAll([5, 10]);
