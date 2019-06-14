2e81441c522c36de73b7e9edb9e3cd1ab222d079
Leah-Grace <leahgdg@hotmail.com>
2019-06-13 23:53:18 -0400

    solved

---
+++ JS_algorithmsDataStructs/intermediateAlgorithm_Scripting/sumAll_Primes.js
@@ -0,0 +1,14 @@
+function sumPrimes(num) {
+  //start with number and
+  //test if it is prime
+
+  //test if number is prime = if number %
+
+  //if prime then add
+  //if not prime then ignore
+
+  return num;
+}
+
+sumPrimes(10); //should return 17
+sumPrimes(977); //should return 73156
