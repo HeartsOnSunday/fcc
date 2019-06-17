6c82a41ec15ac860d20237f0c68b9428ad826554
Leah-Grace <leahgdg@hotmail.com>
2019-06-16 22:53:05 -0400

    solved with .join() and for loop

--- JS_algorithmsDataStructs/basicAlg_Scripting/reverseASTring.js
+++ JS_algorithmsDataStructs/basicAlg_Scripting/reverseASTring.js
@@ -4,13 +4,27 @@ function reverseString(str) {
   console.log(splitstr);
   //check data type = = = Obj;
   let tempArr = [];
-  splitstr.forEach(s => {
-    //console.log(`each letter ${s}`);
-    tempArr.push(s);
-    //push char to an array
-    //.push()- - - most effective order? must be converted in to a string.
-  });
-  console.log(`The temporary Array joined to a string ${tempArr.join("")}`);
+  let indexes = splitstr.length - 1;
+  for (let i = indexes; i >= 0; i--) {
+    console.log(`The index ${i} and the character ${splitstr[i]}`);
+    tempArr.push(splitstr[i]);
+  }
+  //console.log(`${tempArr.toString()}`);
+  //console.log(`each letter ${s}`);
+  /* 
+    console.log(splitstr.indexof(s));
+    tempArr.slice(1, 0, s);
+    */
+
+  //push char to an array
+  //.push()- - - most effective order? No, order returned is the same
+  //use splice to push item of array back into an array?
+  //Splice - - inserting or replacing?
+
+  // use .jon() to return answer as a string
+  str = tempArr.join("");
+  console.log(`${str}`);
+  //console.log(typeof str);
   return str;
 }
 
