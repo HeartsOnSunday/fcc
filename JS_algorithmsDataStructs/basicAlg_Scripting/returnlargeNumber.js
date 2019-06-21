019422b6270c058d51e7f713ba8c72ea93c0c1ca
Leah-Grace <leahgdg@hotmail.com>
2019-06-21 00:42:58 -0400

    this is not working out

--- JS_algorithmsDataStructs/basicAlg_Scripting/returnlargeNumber.js
+++ JS_algorithmsDataStructs/basicAlg_Scripting/returnlargeNumber.js
@@ -1,7 +1,22 @@
 function largestOfFour(arr) {
+  let newArr = 0;
+  let bigNum = [];
   for (let i = 0; i < arr.length; i++) {
-    console.log(`${typeof arr[i]}`);
+    newArr = arr[i][0];
+    for (let j = 1; j < arr[i].length; j++) {
+      if (arr[i][j] > newArr) {
+        // console.log(`${arr[i][j]}`);
+        bigNum.push(arr[i][j]);
+      } else {
+        //console.log(`${arr[i][j - 1]}`);
+        // newArr.push(arr[i][j]);
+      }
+      // console.log(newArr);
+      console.log(`the bigger number: ${bigNum}`);
+      bigNum = [];
+    }
   }
+  console.log(newArr);
   // You can do this!
   return arr;
 }
@@ -12,3 +27,4 @@ largestOfFour([
   [32, 35, 37, 39],
   [1000, 1001, 857, 1]
 ]);
+//incorrectly pushing lower numbers!
